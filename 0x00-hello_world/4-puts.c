53f47371e14fbae5f616d9b9afc358a820b568d5
mostf27 <mostf27@gmail.com>
2022-12-10 10:53:49 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+
+
+/**
+ *
+ * * Main - Entry Point
+ *
+ * *
+ *
+ * * Return: Always 0 (Sucess)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is Like building a multilingual puzzel");
+
+			return(0);
+
+
+
+}
+
+
