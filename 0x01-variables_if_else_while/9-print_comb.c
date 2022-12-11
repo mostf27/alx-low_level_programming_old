1a8e1ae5b8694a4f67ef2f5a9867c97aba7bfd4a
mostf27 <mostf27@gmail.com>
2022-12-11 06:07:23 -0800

    C - Variables, if, else, while Final Project

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	for (ch = 48; ch <= 57; ch++)
 	{
 	putchar(ch);
-	if (ch != 57)
-	}
-	putchar(44);
-	putchar(32);
-	}
+		if (ch != 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
 	}
 	putchar(10); /* this is an ascii code for new line*/
 	return (0);
