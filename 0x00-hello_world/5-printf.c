507e7d5aec2b650b4f594bc137771bd0945444b8
mostf27 <mostf27@gmail.com>
2022-12-10 22:06:40 -0800

    All Projet finialized

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - Entry Point
+ * Return: Always 0 (Sucess)
+  */
+int main (void)
+{
+	printf("With propor gramar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* Main - Entry Point
+* Return: Always 0 (Sucess)
+*/
+int main (void)
+
+{
+
+	printf("Size of a char:%i byte(s)\n", sizeof(char));
+
+	printf("Size of a char:%i byte(s)\n", sizeof(int));
+
+	printf("Size of a char:%i byte(s)\n", sizeof(long int));
+
+	printf("Size of a char:%i byte(s)\n", sizeof(long long int));
+
+	printf("Size of a char:%i byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
+
+
